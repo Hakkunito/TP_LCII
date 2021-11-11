dc7916bdc6c17b18959a1a2ba6c8277cc0879d35
Jey-Code-dude <65753998+Jey-Code-dude@users.noreply.github.com>
2021-11-10 22:01:30 -0300

    funcionalidad hecha, cambien el codigo de conexion y falta cargar las consultas

--- TP_LCII/Form1.Designer.cs
+++ TP_LCII/Form1.Designer.cs
@@ -31,19 +31,19 @@ namespace TP_LCII
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel_Consulta = new System.Windows.Forms.Panel();
-            this.panel_Tabla = new System.Windows.Forms.Panel();
-            this.btn_Consulta1 = new System.Windows.Forms.Button();
-            this.btn_Consulta2 = new System.Windows.Forms.Button();
-            this.btn_Consulta3 = new System.Windows.Forms.Button();
-            this.btn_Consulta4 = new System.Windows.Forms.Button();
             this.btn_Consulta5 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_Consulta4 = new System.Windows.Forms.Button();
+            this.btn_Consulta3 = new System.Windows.Forms.Button();
+            this.btn_Consulta2 = new System.Windows.Forms.Button();
+            this.btn_Consulta1 = new System.Windows.Forms.Button();
+            this.panel_Consulta = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel_Tabla = new System.Windows.Forms.Panel();
+            this.dgvPrincipal = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             this.panel_Consulta.SuspendLayout();
             this.panel_Tabla.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrincipal)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -55,29 +55,83 @@ namespace TP_LCII
             this.panel1.Controls.Add(this.btn_Consulta1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(138, 426);
+            this.panel1.Size = new System.Drawing.Size(158, 568);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // panel_Consulta
+            // btn_Consulta5
             // 
-            this.panel_Consulta.BackColor = System.Drawing.SystemColors.InactiveBorder;
-            this.panel_Consulta.Controls.Add(this.panel2);
-            this.panel_Consulta.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel_Consulta.Location = new System.Drawing.Point(138, 0);
-            this.panel_Consulta.Name = "panel_Consulta";
-            this.panel_Consulta.Size = new System.Drawing.Size(662, 140);
-            this.panel_Consulta.TabIndex = 1;
+            this.btn_Consulta5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Consulta5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Consulta5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Consulta5.FlatAppearance.BorderSize = 2;
+            this.btn_Consulta5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Consulta5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.btn_Consulta5.ForeColor = System.Drawing.Color.White;
+            this.btn_Consulta5.Location = new System.Drawing.Point(0, 452);
+            this.btn_Consulta5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Consulta5.Name = "btn_Consulta5";
+            this.btn_Consulta5.Size = new System.Drawing.Size(158, 113);
+            this.btn_Consulta5.TabIndex = 4;
+            this.btn_Consulta5.Text = "Consulta 5";
+            this.btn_Consulta5.UseVisualStyleBackColor = false;
+            this.btn_Consulta5.Click += new System.EventHandler(this.btn_Consulta5_Click);
             // 
-            // panel_Tabla
+            // btn_Consulta4
             // 
-            this.panel_Tabla.Controls.Add(this.dataGridView1);
-            this.panel_Tabla.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel_Tabla.Location = new System.Drawing.Point(138, 139);
-            this.panel_Tabla.Name = "panel_Tabla";
-            this.panel_Tabla.Size = new System.Drawing.Size(662, 287);
-            this.panel_Tabla.TabIndex = 2;
+            this.btn_Consulta4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Consulta4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Consulta4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Consulta4.FlatAppearance.BorderSize = 2;
+            this.btn_Consulta4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Consulta4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.btn_Consulta4.ForeColor = System.Drawing.Color.White;
+            this.btn_Consulta4.Location = new System.Drawing.Point(0, 339);
+            this.btn_Consulta4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Consulta4.Name = "btn_Consulta4";
+            this.btn_Consulta4.Size = new System.Drawing.Size(158, 113);
+            this.btn_Consulta4.TabIndex = 3;
+            this.btn_Consulta4.Text = "Consulta 4";
+            this.btn_Consulta4.UseVisualStyleBackColor = false;
+            this.btn_Consulta4.Click += new System.EventHandler(this.btn_Consulta4_Click);
+            // 
+            // btn_Consulta3
+            // 
+            this.btn_Consulta3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Consulta3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Consulta3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Consulta3.FlatAppearance.BorderSize = 2;
+            this.btn_Consulta3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Consulta3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.btn_Consulta3.ForeColor = System.Drawing.Color.White;
+            this.btn_Consulta3.Location = new System.Drawing.Point(0, 226);
+            this.btn_Consulta3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Consulta3.Name = "btn_Consulta3";
+            this.btn_Consulta3.Size = new System.Drawing.Size(158, 113);
+            this.btn_Consulta3.TabIndex = 2;
+            this.btn_Consulta3.Text = "Consulta 3";
+            this.btn_Consulta3.UseVisualStyleBackColor = false;
+            this.btn_Consulta3.Click += new System.EventHandler(this.btn_Consulta3_Click);
+            // 
+            // btn_Consulta2
+            // 
+            this.btn_Consulta2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Consulta2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btn_Consulta2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btn_Consulta2.FlatAppearance.BorderSize = 2;
+            this.btn_Consulta2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Consulta2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
+            this.btn_Consulta2.ForeColor = System.Drawing.Color.White;
+            this.btn_Consulta2.Location = new System.Drawing.Point(0, 113);
+            this.btn_Consulta2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Consulta2.Name = "btn_Consulta2";
+            this.btn_Consulta2.Size = new System.Drawing.Size(158, 113);
+            this.btn_Consulta2.TabIndex = 1;
+            this.btn_Consulta2.Text = "Consulta 2";
+            this.btn_Consulta2.UseVisualStyleBackColor = false;
+            this.btn_Consulta2.Click += new System.EventHandler(this.btn_Consulta2_Click);
             // 
             // btn_Consulta1
             // 
@@ -89,106 +143,69 @@ namespace TP_LCII
             this.btn_Consulta1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
             this.btn_Consulta1.ForeColor = System.Drawing.Color.Snow;
             this.btn_Consulta1.Location = new System.Drawing.Point(0, 0);
+            this.btn_Consulta1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Consulta1.Name = "btn_Consulta1";
-            this.btn_Consulta1.Size = new System.Drawing.Size(138, 85);
+            this.btn_Consulta1.Size = new System.Drawing.Size(158, 113);
             this.btn_Consulta1.TabIndex = 0;
             this.btn_Consulta1.Text = "Consulta 1";
             this.btn_Consulta1.UseVisualStyleBackColor = false;
+            this.btn_Consulta1.Click += new System.EventHandler(this.btn_Consulta1_Click);
             // 
-            // btn_Consulta2
+            // panel_Consulta
             // 
-            this.btn_Consulta2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn_Consulta2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_Consulta2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btn_Consulta2.FlatAppearance.BorderSize = 2;
-            this.btn_Consulta2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Consulta2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.btn_Consulta2.ForeColor = System.Drawing.Color.White;
-            this.btn_Consulta2.Location = new System.Drawing.Point(0, 85);
-            this.btn_Consulta2.Name = "btn_Consulta2";
-            this.btn_Consulta2.Size = new System.Drawing.Size(138, 85);
-            this.btn_Consulta2.TabIndex = 1;
-            this.btn_Consulta2.Text = "Consulta 2";
-            this.btn_Consulta2.UseVisualStyleBackColor = false;
-            // 
-            // btn_Consulta3
-            // 
-            this.btn_Consulta3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn_Consulta3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_Consulta3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btn_Consulta3.FlatAppearance.BorderSize = 2;
-            this.btn_Consulta3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Consulta3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.btn_Consulta3.ForeColor = System.Drawing.Color.White;
-            this.btn_Consulta3.Location = new System.Drawing.Point(0, 170);
-            this.btn_Consulta3.Name = "btn_Consulta3";
-            this.btn_Consulta3.Size = new System.Drawing.Size(138, 85);
-            this.btn_Consulta3.TabIndex = 2;
-            this.btn_Consulta3.Text = "Consulta 3";
-            this.btn_Consulta3.UseVisualStyleBackColor = false;
-            // 
-            // btn_Consulta4
-            // 
-            this.btn_Consulta4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn_Consulta4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_Consulta4.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btn_Consulta4.FlatAppearance.BorderSize = 2;
-            this.btn_Consulta4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Consulta4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.btn_Consulta4.ForeColor = System.Drawing.Color.White;
-            this.btn_Consulta4.Location = new System.Drawing.Point(0, 255);
-            this.btn_Consulta4.Name = "btn_Consulta4";
-            this.btn_Consulta4.Size = new System.Drawing.Size(138, 85);
-            this.btn_Consulta4.TabIndex = 3;
-            this.btn_Consulta4.Text = "Consulta 4";
-            this.btn_Consulta4.UseVisualStyleBackColor = false;
-            // 
-            // btn_Consulta5
-            // 
-            this.btn_Consulta5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn_Consulta5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.btn_Consulta5.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.btn_Consulta5.FlatAppearance.BorderSize = 2;
-            this.btn_Consulta5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Consulta5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel);
-            this.btn_Consulta5.ForeColor = System.Drawing.Color.White;
-            this.btn_Consulta5.Location = new System.Drawing.Point(0, 340);
-            this.btn_Consulta5.Name = "btn_Consulta5";
-            this.btn_Consulta5.Size = new System.Drawing.Size(138, 85);
-            this.btn_Consulta5.TabIndex = 4;
-            this.btn_Consulta5.Text = "Consulta 5";
-            this.btn_Consulta5.UseVisualStyleBackColor = false;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(662, 287);
-            this.dataGridView1.TabIndex = 0;
+            this.panel_Consulta.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel_Consulta.Controls.Add(this.panel2);
+            this.panel_Consulta.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_Consulta.Location = new System.Drawing.Point(158, 0);
+            this.panel_Consulta.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel_Consulta.Name = "panel_Consulta";
+            this.panel_Consulta.Size = new System.Drawing.Size(756, 187);
+            this.panel_Consulta.TabIndex = 1;
             // 
             // panel2
             // 
             this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
             this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(535, 0);
+            this.panel2.Location = new System.Drawing.Point(611, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(127, 140);
+            this.panel2.Size = new System.Drawing.Size(145, 187);
             this.panel2.TabIndex = 0;
+            // 
+            // panel_Tabla
+            // 
+            this.panel_Tabla.Controls.Add(this.dgvPrincipal);
+            this.panel_Tabla.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel_Tabla.Location = new System.Drawing.Point(158, 185);
+            this.panel_Tabla.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel_Tabla.Name = "panel_Tabla";
+            this.panel_Tabla.Size = new System.Drawing.Size(756, 383);
+            this.panel_Tabla.TabIndex = 2;
+            // 
+            // dgvPrincipal
+            // 
+            this.dgvPrincipal.BackgroundColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.dgvPrincipal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.dgvPrincipal.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvPrincipal.Name = "dgvPrincipal";
+            this.dgvPrincipal.RowHeadersWidth = 51;
+            this.dgvPrincipal.RowTemplate.Height = 25;
+            this.dgvPrincipal.Size = new System.Drawing.Size(756, 383);
+            this.dgvPrincipal.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.WindowFrame;
-            this.ClientSize = new System.Drawing.Size(800, 426);
+            this.ClientSize = new System.Drawing.Size(914, 568);
             this.Controls.Add(this.panel_Tabla);
             this.Controls.Add(this.panel_Consulta);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Concesionaria LCII-CAR";
@@ -197,7 +214,7 @@ namespace TP_LCII
             this.panel1.ResumeLayout(false);
             this.panel_Consulta.ResumeLayout(false);
             this.panel_Tabla.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPrincipal)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -212,7 +229,7 @@ namespace TP_LCII
         private System.Windows.Forms.Button btn_Consulta5;
         private System.Windows.Forms.Button btn_Consulta4;
         private System.Windows.Forms.Button btn_Consulta3;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvPrincipal;
         private System.Windows.Forms.Panel panel2;
     }
 }
--- TP_LCII/Form1.cs
+++ TP_LCII/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace TP_LCII
 {
@@ -30,6 +31,53 @@ namespace TP_LCII
         private void panel_Consulta_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+        public void CargarUnDatagrid(string consulta)
+        {
+            // cambiar el codigo de la conexion 
+            SqlConnection cn = new SqlConnection(@"Data Source=DESKTOP-CBD3HB7\SQLEXPRESS;Initial Catalog=Fabrica;Integrated Security=True");
+            try
+            {
+                
+                cn.Open();
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(consulta, cn);
+                DataTable dt = new DataTable();
+
+                dataAdapter.Fill(dt);
+                dgvPrincipal.DataSource = dt;                                    
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cn.Close();
+            }
+        }
+        // faltan las consultas
+        private void btn_Consulta1_Click(object sender, EventArgs e)
+        {
+            CargarUnDatagrid("");
+        }
+
+        private void btn_Consulta2_Click(object sender, EventArgs e)
+        {
+            CargarUnDatagrid("");
+        }
+
+        private void btn_Consulta3_Click(object sender, EventArgs e)
+        {
+            CargarUnDatagrid("");
+        }
+
+        private void btn_Consulta4_Click(object sender, EventArgs e)
+        {
+            CargarUnDatagrid("");
+        }
+
+        private void btn_Consulta5_Click(object sender, EventArgs e)
+        {
+            CargarUnDatagrid("");
         }
     }
 }
